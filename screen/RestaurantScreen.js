0c428e073178a132099a8970fd8bc90a472ad5bf
Pawan.Kumar2 <Pawan.Kumar2@rsystems.com>
2021-07-29 22:54:24 +0530

    my work desc

--- screen/RestaurantScreen.js
+++ screen/RestaurantScreen.js
@@ -112,24 +112,37 @@ const RestaurantScreen = ({ route, navigation }) => {
           }}>
             <View style={{ flexDirection: 'row' }} >
               <Image source={{ uri: item.avatar }} style={{ height: 126, width: 110, borderRadius: 10 }} />
-              <View style={{ paddingStart: 13 }}>
-                <Text style={{ fontSize: 16, color: '#0A0A0A' }}>{item.first_name}</Text>
-                <Text style={{ fontSize: 12, color: '#ABAFB3' }}>{item.last_name}</Text>
-                <StarRating
-                  starSize={20}
-                  halfStarEnabled={false}
-                  disabled={false}
-                  maxStars={5}
-                  rating={item.id}
-                  selectedStar={(rating) => {
-                    var index = restaurantData.indexOf(item);
-                    restaurantData[index].id = rating;
-                    setRestaurantData(restaurantData)
-                    setRender(!isRender)
-                  }}
-                  emptyStar={require("../assets/empty_star.png")}
-                  fullStar={require("../assets/fill_star.png")}
-                />
+              <View style={{ flex: 1, justifyContent: 'space-between', margin: 10 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Text style={{ fontSize: 16, color: '#0A0A0A' }}>{item.first_name}</Text>
+                  <Text style={{ fontSize: 16, color: '#0A0A0A', alignItems: 'flex-end' }}>5 KM </Text>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Text style={{ fontSize: 16, color: '#0A0A0A' }}>{item.last_name}</Text>
+                  <Image source={require("../assets/heart_filled.png")} style={{ height: 22, width: 22 }} />
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+
+                  <View style={{ width: '50%' }}
+                  ><StarRating
+                      starSize={20}
+                      halfStarEnabled={false}
+                      disabled={false}
+                      maxStars={5}
+                      rating={item.id}
+                      selectedStar={(rating) => {
+                        var index = restaurantData.indexOf(item);
+                        restaurantData[index].id = rating;
+                        setRestaurantData(restaurantData)
+                        setRender(!isRender)
+                      }}
+                      emptyStar={require("../assets/empty_star.png")}
+                      fullStar={require("../assets/fill_star.png")}
+                    /></View>
+                  <Image source={require("../assets/comment_filled.png")} style={{ height: 22, width: 22 }} />
+
+                </View>
+
               </View>
             </View>
           </TouchableOpacity>
