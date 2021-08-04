0b452c7a324624ed83157683b02e289e30eca6b7
Pawan.Kumar2 <Pawan.Kumar2@rsystems.com>
2021-08-04 20:32:26 +0530

    googlemap

--- screen/Map.js
+++ screen/Map.js
@@ -1,6 +1,27 @@
-import React, {Component} from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React from 'react';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 export default class Map extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      markers: [
+        {
+          title: 'hello',
+          coordinates: {
+            latitude: 78.1351,
+            longitude: 78.1351,
+          },
+        },
+        {
+          title: 'hello',
+          coordinates: {
+            latitude: 29.3732,
+            longitude: 78.1351,
+          },
+        },
+      ],
+    };
+  }
   render() {
     return (
       <MapView
@@ -12,8 +33,9 @@ export default class Map extends React.Component {
           longitude: 78.1351,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
-        }}
-      />
+        }}>
+        <Marker coordinate={{latitude: 29.3732, longitude: 78.1351}} />
+      </MapView>
     );
   }
 }
